b17202f3f8202ba0e33c004a12a88a0672e6de57
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2026-04-16 00:26:01 -0500

    Fix errors.

--- pi4_boot/src/mailbox.h
+++ pi4_boot/src/mailbox.h
@@ -1,3 +1,3 @@
 #include <stdint.h>
 
-#define GET_MAILBOX_ADDR(addr, chan) (((addr) & ~0xF) | (chan & 0xF))
+#define GET_MBOX_ADDR(addr, chan) (((addr) & ~0xF) | (chan & 0xF))
--- pi4_boot/src/mmio_util.h
+++ pi4_boot/src/mmio_util.h
@@ -1,23 +1,23 @@
 #include <stdint.h>
 
 
-static inline void mmio_write_uint8_t(uint8_t addr, uint8_t val)
+static inline void mmio_write_uint8_t(uint32_t addr, uint8_t val)
 {
     *(volatile uint8_t *)(addr) = val;
 } 
 
-static inline uint8_t mmio_read_uint8_t(uint8_t addr)
+static inline uint8_t mmio_read_uint8_t(uint32_t addr)
 {
     return *(volatile uint8_t *)(addr);
 } 
 
 
-static inline void mmio_write_uint16_t(uint16_t addr, uint16_t val)
+static inline void mmio_write_uint16_t(uint32_t addr, uint16_t val)
 {
     *(volatile uint16_t *)(addr) = val;
 } 
 
-static inline uint16_t mmio_read_uint16_t(uint16_t addr)
+static inline uint16_t mmio_read_uint16_t(uint32_t addr)
 {
     return *(volatile uint16_t *)(addr);
 } 
